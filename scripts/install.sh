951d136db4c2db5f31c82817ee799f57eeba61b7
CLM-user <gomezyurlei13@gmail.com>
2025-12-10 01:34:54 +0000

    again

--- scripts/install.sh
+++ scripts/install.sh
@@ -21,5 +21,5 @@ echo "Succesfully installed!"
 sleep 1
 echo "Script will auto destruct"
 sleep 1
-( sleep 1 && rm -rf ~/easy-arch-cmd/scripts/install.sh ) &
+( sleep 1 && rm -rf "$0" ) &
 echo "ok bye"
